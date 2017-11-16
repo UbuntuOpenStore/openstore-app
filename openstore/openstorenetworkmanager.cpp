f93962c9ac182fa659e55f29d83d5550ed19c106
Stefano Verzegnassi <verzegnassi.stefano@gmail.com>
2017-11-16 22:42:07 +0100

    Remove debug

--- openstore/openstorenetworkmanager.cpp
+++ openstore/openstorenetworkmanager.cpp
@@ -51,7 +51,7 @@ QNetworkReply *OpenStoreNetworkManager::sendRequest(QNetworkRequest request)
     url.setQuery(q);
     request.setUrl(url);
 
-    qDebug() << "Firing request for" << request.url();
+    // qDebug() << "Firing request for" << request.url();
 
     return m_manager->get(request);
 }

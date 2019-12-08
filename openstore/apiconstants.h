b182eab338e60b9775d5eb1056e2611fbf6e10f5
Johannes Renkl <hummlbach@web.de>
2019-12-08 17:36:58 +0100

    Fix review endpoints

--- openstore/apiconstants.h
+++ openstore/apiconstants.h
@@ -1,16 +1,17 @@
 #ifndef APICONSTANTS_H
 #define APICONSTANTS_H
 
-#define STORE_DOMAIN QStringLiteral("open-store.io")
-#define API_BASEURL QStringLiteral("https://open-store.io/")
+#define STORE_DOMAIN QStringLiteral("openteststore.tk")
+#define API_BASEURL QStringLiteral("https://openteststore.tk/")
 
 #define API_DISCOVER_ENDPOINT QStringLiteral("api/v3/discover")
 #define API_APPDETAILS_ENDPOINT QStringLiteral("api/v3/apps/%1")
 #define API_SEARCH_ENDPOINT QStringLiteral("api/v3/apps")
 #define API_CATEGORIES_ENDPOINT QStringLiteral("api/v3/categories")
 #define API_REVISION_ENDPOINT QStringLiteral("api/v3/revisions")
-#define API_REVIEW_LIST_ENDPOINT API_APPDETAILS_ENDPOINT + QStringLiteral("/reviews")
-#define API_REVIEW_ENDPOINT API_APPDETAILS_ENDPOINT + QStringLiteral("/reviews/%2")
-#define API_REVIEW_COMMENT_ENDPOINT API_APPDETAILS_ENDPOINT + QStringLiteral("/reviews/%2/comment")
+
+#define API_REVIEW_LIST_ENDPOINT QStringLiteral("api/v3/apps/%1/reviews")
+#define API_REVIEW_ENDPOINT QStringLiteral("api/v3/apps/%1/reviews/%2")
+#define API_REVIEW_COMMENT_ENDPOINT QStringLiteral("api/v3/apps/%1/reviews/%2/comment")
 
 #endif // APICONSTANTS_H

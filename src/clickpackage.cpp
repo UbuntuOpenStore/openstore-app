70a31ac9d67e53e8da3e74f02f65547fae62195c
Alfred Neumayer <dev.beidl@gmail.com>
2026-06-14 14:27:39 +0200

    src: Fix local-only Click & Snap app removal
    Fix the distinction between local Clicks and Snaps by adding a 'packageType' role
    and member to LocalPackagesModel and LocalPackageItem, respectively. Also ensure that,
    when we instantiate ClickPackage and SnapPackage objects, the packageType is set
    appropriately.

--- src/clickpackage.cpp
+++ src/clickpackage.cpp
@@ -159,7 +159,7 @@ void ClickPackageItem::fillData(const QVariantMap& json)
   m_updatedDate = json.value("updated_date").toDateTime();
   m_channels = json.value("channels").toStringList();
   m_types = json.value("types").toStringList();
-  m_packageType = json.value("package_type").toString();
+  m_packageType = QStringLiteral("click");
 
   QStringList deviceCompatibilities = json.value("device_compatibilities").toStringList();
   m_matchingFramework = false;

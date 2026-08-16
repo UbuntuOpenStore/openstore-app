609f312fdc712d17db82d9decd476c717246d76a
Brian Douglass <bhdouglass@gmail.com>
2026-08-15 22:41:13 -0400

    Updated to use v4 api endpoints

--- src/apiconstants.h
+++ src/apiconstants.h
@@ -24,11 +24,11 @@
 #define API_DISCOVER_ENDPOINT QStringLiteral("api/v4/discover")
 #define API_APPDETAILS_ENDPOINT QStringLiteral("api/v4/apps/%1")
 #define API_SEARCH_ENDPOINT QStringLiteral("api/v4/apps")
-#define API_CATEGORIES_ENDPOINT QStringLiteral("api/v3/categories")
+#define API_CATEGORIES_ENDPOINT QStringLiteral("api/v4/categories")
 #define API_REVISION_ENDPOINT QStringLiteral("api/v4/revisions")
 
-#define API_REVIEW_LIST_ENDPOINT QStringLiteral("api/v3/apps/%1/reviews")
-#define API_REVIEW_ENDPOINT QStringLiteral("api/v3/apps/%1/reviews/%2")
-#define API_REVIEW_COMMENT_ENDPOINT QStringLiteral("api/v3/apps/%1/reviews/%2/comment")
+#define API_REVIEW_LIST_ENDPOINT QStringLiteral("api/v4/apps/%1/reviews")
+#define API_REVIEW_ENDPOINT QStringLiteral("api/v4/apps/%1/reviews/%2")
+#define API_REVIEW_COMMENT_ENDPOINT QStringLiteral("api/v4/apps/%1/reviews/%2/comment")
 
 #endif // APICONSTANTS_H
